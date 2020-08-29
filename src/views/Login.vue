a895dc2828f684024ddf771f59687a11ddb53d8d
Victoria Sapronova <v.sapronova@gmail.com>
2020-08-28 23:14:28 -0400

    Added login

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="form_login">
+        <form method="post">
+            <div class="form-group">
+                <input v-model="username" autocomplete="off" autofocus class="form-control" name="username" placeholder="Username" type="text">
+            </div>
+            <div class="form-group">
+                <input v-model="password" class="form-control" name="password" placeholder="Password" type="password">
+            </div>
+            <button @click.stop.prevent="submit()" class="btn btn-primary" type="submit">Log In</button>
+        </form>
+        <div id="error" v-for="error in errors" style="margin: 50px; width: 300px; margin-left: 380px" class="alert alert-danger" role="alert" >
+                    {{ error }}
+        </div>
+    </div>
+</template>
